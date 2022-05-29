e501e78f2b10341097362505ca2fce6a565fbb55
Lucas Gomes <96914640+lucas-glopes@users.noreply.github.com>
2022-05-29 11:53:22 -0300

    Format do index.html, scripts,js e style.css

--- Projetos em Andamento/ToDo List/Modelos HTML/js/script.js
+++ Projetos em Andamento/ToDo List/Modelos HTML/js/script.js
@@ -103,38 +103,29 @@ document
     });
 
 // função de abrir pop up para excluir tarefa
-function excluirTarefa() { 
-    document.querySelector('.delete-alert')
-        .style.pointerEvents = 'all'
+function excluirTarefa() {
+    document.querySelector(".delete-alert").style.pointerEvents = "all";
 
-    document.querySelector('.black-background')
-        .style.opacity = '.95'
+    document.querySelector(".black-background").style.opacity = ".95";
 
-    document.querySelector('.alert')
-        .style.opacity = '1'
-    document.querySelector('.alert')
-        .style.transform = 'scale(1)'
+    document.querySelector(".alert").style.opacity = "1";
+    document.querySelector(".alert").style.transform = "scale(1)";
 
     // bloquear scroll
-    document.querySelector('body')
-        .style.overflowY = 'hidden'
+    document.querySelector("body").style.overflowY = "hidden";
 }
 
 // função para fechar o pop up de excluir tarefa
-document.querySelector('.alert-buttons button:first-child')
-    .addEventListener('click', () => {
-        document.querySelector('.delete-alert')
-        .style.pointerEvents = 'none'
+document
+    .querySelector(".alert-buttons button:first-child")
+    .addEventListener("click", () => {
+        document.querySelector(".delete-alert").style.pointerEvents = "none";
 
-        document.querySelector('.black-background')
-            .style.opacity = '0'
+        document.querySelector(".black-background").style.opacity = "0";
 
-        document.querySelector('.alert')
-            .style.opacity = '0'
-        document.querySelector('.alert')
-            .style.transform = 'scale(.9)'
+        document.querySelector(".alert").style.opacity = "0";
+        document.querySelector(".alert").style.transform = "scale(.9)";
 
         // liberar scroll
-        document.querySelector('body')
-        .style.overflowY = 'auto'
-    })
+        document.querySelector("body").style.overflowY = "auto";
+    });
